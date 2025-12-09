d07fe60ed77c4b9ff05b6117d7a6655f74358883
mohammad <mohammad.goroohi0582@gmail.com>
2025-12-09 21:00:52 +0330

    feat : add dynamic model editor

--- Models/Shared/PropertyDescriptor.cs
+++ Models/Shared/PropertyDescriptor.cs
@@ -1,17 +1,11 @@
 ﻿namespace QuizWebAssembly.Models.Shared
 {
-    public class ModelEditor
-    {
-        public string? Title { get; set; }
-        public bool IsShow { get; set; }
-        public List<PropertyDescriptor> Fields { get; set; } = new List<PropertyDescriptor>();
-    }
     public class PropertyDescriptor
     {
         public string? Name { get; set; }
         public string? DisplayName { get; set; }
         public ModelEditorValueType ValuType { get; set; }
-        public object Value { get; set; }
+        public string Value { get; set; }
     }
     public enum ModelEditorValueType
     {
--- Services/EditorModalService.cs
+++ Services/EditorModalService.cs
@@ -4,6 +4,11 @@ namespace QuizWebAssembly.Services
 {
     public class EditorModalService
     {
-        public Action<ModelEditor> OpenModal { get; set; }
+        public bool IsShow { get; set; } = false;
+        public string Title { get; set; } = string.Empty;
+        public List<PropertyDescriptor> Fields { get; set; } = new List<PropertyDescriptor>();
+        public Action<List<PropertyDescriptor>>? OpenModal;
+        public Action? CloseModal;
+        public Action? OnConfirmClick { get; set; }
     }
 }
